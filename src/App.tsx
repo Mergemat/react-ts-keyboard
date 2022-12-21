0b3c174c91df190f08d7ff2e65a1e2a876126742
Mergemat <38819496+Mergemat@users.noreply.github.com>
2022-12-21 09:33:16 +0300

    chore: changed setKeyLan prop

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,14 @@ import LanButtons from './components/LanButtons';
 
 const App = () => {
   const [lan, setLan] = useState('RU');
-
-  const setKeyLan = (l: string) => {
-    setLan(l);
-  };
-
+  
   return (
     <div
       onFocus={(event) => event.stopPropagation()}
       className="flex h-screen flex-col items-center justify-center space-y-10 bg-slate-900"
     >
       <Keyboard lan={lan} />
-      <LanButtons lan={lan} setKeyLan={(l: string) => setKeyLan(l)} />
+      <LanButtons lan={lan} setKeyLan={(l: string) => setLan(l)} />
     </div>
   );
 };
